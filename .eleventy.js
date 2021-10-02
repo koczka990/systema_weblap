c0b29df89a4b7430db7f2e752d3fcbec66781f5f
Koczka Vencel <koczka990@gmail.com>
2021-10-02 22:55:26 +0200

    corrected .eleventy.js to send through yml

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy('./src/app.css');
     eleventyConfig.addPassthroughCopy('./src/imgs');
     eleventyConfig.addPassthroughCopy('./src/pdf');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
     return {
         dir: {
